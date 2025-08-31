cb3a45db5cc627789992b6155cb159bd4552c6cc
moisespinedo24 <za20012277@zapopan.tecmm.edu.mx>
2025-08-30 22:19:00 -0600

    inicio

--- app/src/main/java/com/example/mvvmloigin/ui/login/ui/LoginScreen.kt
+++ app/src/main/java/com/example/mvvmloigin/ui/login/ui/LoginScreen.kt
@@ -1,10 +1,15 @@
 package com.example.mvvmloigin.ui.login.ui
 
+import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,9 +29,16 @@ import com.example.mvvmloigin.R
     fun Login(modifier: Modifier) {
         Column(modifier = modifier) {
             HeaderImage()
+            Spacer(modifier = Modifier.padding(16.dp))
+            EmailField()
 
         }
     }
+
+@Composable
+fun EmailField() {
+    TextField(values = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
+}
 
 @Composable
 fun HeaderImage() {
