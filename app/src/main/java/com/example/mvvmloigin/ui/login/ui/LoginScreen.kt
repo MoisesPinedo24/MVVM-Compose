bfea1ba5e363b8e431e0d5f0de6fbbf6188b2af9
moisespinedo24 <za20012277@zapopan.tecmm.edu.mx>
2025-08-30 22:54:44 -0600

    upgrade

--- app/src/main/java/com/example/mvvmloigin/ui/login/ui/LoginScreen.kt
+++ app/src/main/java/com/example/mvvmloigin/ui/login/ui/LoginScreen.kt
@@ -1,6 +1,5 @@
 package com.example.mvvmloigin.ui.login.ui
 
-import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,12 +7,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mvvmloigin.R
 
@@ -28,20 +33,33 @@ import com.example.mvvmloigin.R
     @Composable
     fun Login(modifier: Modifier) {
         Column(modifier = modifier) {
-            HeaderImage()
+            HeaderImage(Modifier.align(Alignment.CenterHorizontally))
             Spacer(modifier = Modifier.padding(16.dp))
             EmailField()
 
         }
     }
-
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun EmailField() {
-    TextField(values = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
+    TextField(value = "", onValueChange = {},
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email")},
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.colors(focusedTextColor = Color(0xFF777777),
+            unfocusedTextColor = Color(0xFFFFFFFF),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
 }
 
+
 @Composable
-fun HeaderImage() {
+fun HeaderImage(modifier: Modifier) {
     Image(painter = painterResource(id = R.drawable.mvvm ), contentDescription = "Header" )
 }
 
